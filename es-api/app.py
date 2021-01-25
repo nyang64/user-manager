b0931af50cdc6662ffe5b5309f9198601502142f
yogendra12345 <yogendra.rai@infostretch.com>
2021-01-25 13:54:21 +0530

    fix refresh token

--- es-api/app.py
+++ es-api/app.py
@@ -16,7 +16,6 @@ app.config["PROPAGATE_EXCEPTIONS"] = True
 main_bp = Blueprint('api', __name__)
 api = Api(main_bp)
 app.register_blueprint(main_bp)
-<<<<<<< HEAD
 
 '''
 @app.before_first_request
@@ -30,7 +29,6 @@ ma.init_app(app)
 migrate.init_app(app, db)
 
 
->>>>>>> 065d703665fe9c5be8ffdc9cb68403dc7077e3c6
 api.add_resource(UserRegister, "/register")
 api.add_resource(UserLogin, "/login")
 api.add_resource(UpdateUserPassword, "/updatepassword")

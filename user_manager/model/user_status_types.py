13c51fe202a283b205d484326499f761a8b6cf8e
Laura Kirby <laura@elementsci.com>
2021-05-05 14:03:11 -0700

    ESSW-330: add seed data for populating user_status_types

--- user_manager/model/user_status_types.py
+++
@@ -1,8 +0,0 @@
-from db import db
-from model.base_model import BaseModel
-
-
-class UserStatusType(BaseModel):
-    __tablename__ = "user_status_types"
-    __table_args__ = {"schema": "ES"}
-    name = db.Column("name", db.String(30), nullable=False)

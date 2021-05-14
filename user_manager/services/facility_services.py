fc3c80cfc82fcb54c067d485ac1a5c4eeb53f1fa
Laura Kirby <laura@elementsci.com>
2021-05-14 13:06:17 -0700

    fix facility creation.
    - update accessors to work with an instance of Address instead of a dict

--- user_manager/services/facility_services.py
+++ user_manager/services/facility_services.py
@@ -1,9 +1,10 @@
-from werkzeug.exceptions import InternalServerError
-from services.repository.db_repositories import DbRepository
-from model.facilities import Facilities
-from model.address import Address
-from sqlalchemy import exc
 import logging
+
+from model.address import Address
+from model.facilities import Facilities
+from services.repository.db_repositories import DbRepository
+from sqlalchemy import exc
+from werkzeug.exceptions import InternalServerError
 
 
 class FacilityService(DbRepository):
@@ -11,50 +12,44 @@ class FacilityService(DbRepository):
         pass
 
     def register_facility(self, address, facility_name, on_call_phone):
-        ''' Commit the transcation'''
-        logging.info('Transcation Started.')
+        """ Commit the transcation"""
+        logging.info("Transcation Started.")
         try:
             address_id = self.save_address(address)
             facility_id = self.save_facility(facility_name, address_id, on_call_phone)
             self.commit_db()
-            logging.info('Transcation Completed')
+            logging.info("Transcation Completed")
             return address_id, facility_id
         except exc.SQLAlchemyError as error:
             raise InternalServerError(str(error))
 
     def save_address(self, address):
-        '''Flush the address transcation'''
-        logging.info('Binding Address Data')
+        """Flush the address transcation"""
+        logging.info("Binding Address Data")
         try:
-            addr = Address(street_address_1=address.get('street_address_1'),
-                           street_address_2=address.get('street_address_2'),
-                           city=address.get('city'),
-                           state=address.get('state'),
-                           country=address.get('country'),
-                           postal_code=address.get('postal_code'))
-            self.flush_db(addr)
-            logging.info('Flushed the Address data')
-            if addr.id is None:
-                logging.error('Failed to Save Address')
-                raise exc.SQLAlchemyError('Error while adding address')
-            return addr.id
+            self.flush_db(address)
+            logging.info("Flushed the Address data")
+            if address.id is None:
+                logging.error("Failed to Save Address")
+                raise exc.SQLAlchemyError("Error while adding address")
+            return address.id
         except exc.SQLAlchemyError as error:
-            logging.error('Error Occured {}'.format(str(error)))
+            logging.error("Error Occured {}".format(str(error)))
             raise InternalServerError(str(error))
 
     def save_facility(self, facility_name, address_id, on_call_phone):
-        ''' Flush the Facility transcation'''
-        logging.info('Binding Facility Data')
+        """ Flush the Facility transcation"""
+        logging.info("Binding Facility Data")
         try:
-            facilities = Facilities(address_id=address_id,
-                                    name=facility_name,
-                                    on_call_phone=on_call_phone)
+            facilities = Facilities(
+                address_id=address_id, name=facility_name, on_call_phone=on_call_phone
+            )
             self.flush_db(facilities)
-            logging.info('Flushed the Facility data')
+            logging.info("Flushed the Facility data")
             if facilities.id is None:
-                logging.error('Failed to Save Facility')
-                raise exc.SQLAlchemyError('Error while adding facility')
+                logging.error("Failed to Save Facility")
+                raise exc.SQLAlchemyError("Error while adding facility")
             return facilities.id
         except exc.SQLAlchemyError as error:
-            logging.error('Error Occured {}'.format(str(error)))
+            logging.error("Error Occured {}".format(str(error)))
             raise InternalServerError(str(error))
